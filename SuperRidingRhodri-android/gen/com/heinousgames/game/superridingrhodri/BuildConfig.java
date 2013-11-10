da545d3d9ec31dbc226803e4303295292285b8a3
Steven Hanus <ministeve412@gmail.com>
2013-11-10 14:52:34 -0500

    initializing new rhodri project

---
+++ SuperRidingRhodri-android/gen/com/heinousgames/game/superridingrhodri/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.heinousgames.game.superridingrhodri;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
